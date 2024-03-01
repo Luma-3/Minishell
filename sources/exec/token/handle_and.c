5e795dedcd5bc3cba6b188cc7471a678cfe99462
Jean-Baptiste Brousse <jbrousse@paul-f5Br6s4.clusters.42paris.fr>
2024-03-01 10:40:54 +0100

    add builtins for all token

--- sources/exec/token/handle_and.c
+++ sources/exec/token/handle_and.c
@@ -6,11 +6,12 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:07:16 by antgabri          #+#    #+#             */
-/*   Updated: 2024/02/29 10:43:05 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/03/01 10:39:23 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+#include "ms_builtins.h"
 
 int	skip_next_cmd(t_prompt *prompt)
 {
@@ -43,6 +44,8 @@ int	handle_and(t_prompt *prompt, t_child *childs,
 				prompt->current_index, prompt->pos_after_token - 1);
 		if (tab_cmd == NULL)
 			return (FAILURE);
+		if (is_builtins(tab_cmd[0]) == true)
+			return (exec_builtins(tab_cmd, prompt));
 		if (exec_command(tab_cmd, prompt->env) == FAILURE)
 			exit (EXIT_FAILURE);
 	}
--- sources/exec/token/handle_pipe.c
+++ sources/exec/token/handle_pipe.c
@@ -6,11 +6,12 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:03:33 by antgabri          #+#    #+#             */
-/*   Updated: 2024/02/29 10:41:17 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/03/01 10:39:35 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+#include "ms_builtins.h"
 
 static void	dup2_write_pipe(t_child *childs, int index_child)
 {
@@ -41,6 +42,8 @@ int	handle_pipe(t_prompt *prompt, t_child *childs,
 				prompt->current_index, prompt->pos_after_token - 1);
 		if (tab_cmd == NULL)
 			return (FAILURE);
+		if (is_builtins(tab_cmd[0]) == true)
+			return (exec_builtins(tab_cmd, prompt));
 		if (exec_command(tab_cmd, prompt->env) == FAILURE)
 			exit(EXIT_FAILURE);
 
