9d96c1fd7f260921e0b72ee9492a72f9dfa22526
Jean-Baptiste Brousse <jbrousse@paul-f5Br6s5.clusters.42paris.fr>
2024-04-04 14:07:06 +0200

    signal ok

--- sources/components/display/display.c
+++ sources/components/display/display.c
@@ -6,7 +6,7 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 10:20:41 by anthony           #+#    #+#             */
-/*   Updated: 2024/04/04 12:02:44 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:53:47 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,21 +64,13 @@ char	*shell_prompt(t_maindata *core_data)
 	char	*line;
 
 	line = PROMPT_SHELL;
-	if (g_sigreciever != SIGINT)
-	{
-		prompt = create_sh_prompt(core_data->env, core_data->uname,
-			core_data->last_status);
-		if (prompt != NULL)
-			ft_putendl_fd(prompt, 1);
-		else
-			ft_putendl_fd("| Kikishell |", 1);
-		free(prompt);
-		input = readline(line);
-	}
+	prompt = create_sh_prompt(core_data->env, core_data->uname,
+		core_data->last_status);
+	if (prompt != NULL)
+		ft_putendl_fd(prompt, 1);
 	else
-	{
-		g_sigreciever = 0;
-		input = readline("");
-	}
+		ft_putendl_fd("| Kikishell |", 1);
+	free(prompt);
+	input = readline(line);
 	return (input);
 }
